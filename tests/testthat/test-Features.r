965b3c6cdcecfb043a3322e98cc3447bffaaf311
Joseph Luchman <jluchman@gmail.com>
2021-09-05 09:45:47 -0500

    consmodel and reverse tests

--- tests/testthat/test-Features.r
+++ tests/testthat/test-Features.r
@@ -84,7 +84,7 @@ cdl_test2 <- matrix(c(vs_c2, cyl_c2, carb_c2), nrow = 3, ncol = 3,
                    byrow = TRUE, dimnames = cdl_names2)
 
 test_obj2 <- domin(mpg ~ vs + cyl + carb, "lm", list("summary", "r.squared"), 
-                  data = mtcars, all = list(c("am")))
+                  data = mtcars, all = c("am"))
 
 test_that("Test Use of All with Conditional Dominance", {
   expect_equal(test_obj2$Conditional_Dominance, cdl_test2
@@ -103,3 +103,87 @@ test_that("Test Complete Dominance as \"off\"", {
   expect_null(test_obj3$Complete_Dominance
   )})
 
+# here constant model + reverse
+
+vs_cns_mgn <- c( extractAIC(lm(mpg ~ vs, data = mtcars))[[2]] - 
+                   extractAIC(lm(mpg ~ 1, data = mtcars))[[2]],
+                 extractAIC(lm(mpg ~ vs + cyl, data = mtcars))[[2]] - 
+                   extractAIC(lm(mpg ~ cyl, data = mtcars))[[2]], 
+                 extractAIC(lm(mpg ~ vs + carb, data = mtcars))[[2]] - 
+                   extractAIC(lm(mpg ~ carb, data = mtcars))[[2]], 
+                 extractAIC(lm(mpg ~ vs + cyl + carb, data = mtcars))[[2]] - 
+                   extractAIC(lm(mpg ~ cyl + carb, data = mtcars))[[2]] )
+
+cyl_cns_mgn <- c( extractAIC(lm(mpg ~ cyl, data = mtcars))[[2]]- 
+                    extractAIC(lm(mpg ~ 1, data = mtcars))[[2]],
+                 extractAIC(lm(mpg ~ vs + cyl, data = mtcars))[[2]] - 
+                   extractAIC(lm(mpg ~ vs, data = mtcars))[[2]], 
+                 extractAIC(lm(mpg ~ cyl + carb, data = mtcars))[[2]] - 
+                   extractAIC(lm(mpg ~ carb, data = mtcars))[[2]], 
+                 extractAIC(lm(mpg ~ vs + cyl + carb, data = mtcars))[[2]] - 
+                   extractAIC(lm(mpg ~ vs + carb, data = mtcars))[[2]] )
+
+carb_cns_mgn <- c( extractAIC(lm(mpg ~ carb, data = mtcars))[[2]]- 
+                    extractAIC(lm(mpg ~ 1, data = mtcars))[[2]],
+                  extractAIC(lm(mpg ~ vs + carb, data = mtcars))[[2]] - 
+                    extractAIC(lm(mpg ~ vs, data = mtcars))[[2]], 
+                  extractAIC(lm(mpg ~ cyl + carb, data = mtcars))[[2]] - 
+                    extractAIC(lm(mpg ~ cyl, data = mtcars))[[2]], 
+                  extractAIC(lm(mpg ~ vs + cyl + carb, data = mtcars))[[2]] - 
+                   extractAIC(lm(mpg ~ vs + cyl, data = mtcars))[[2]] )
+
+vs_cns_c <- c(vs_cns_mgn[[1]], mean(vs_cns_mgn[2:3]), vs_cns_mgn[[4]])
+cyl_cns_c <- c(cyl_cns_mgn[[1]], mean(cyl_cns_mgn[2:3]), cyl_cns_mgn[[4]])
+carb_cns_c <- c(carb_cns_mgn[[1]], mean(carb_cns_mgn[2:3]), carb_cns_mgn[[4]])
+
+cdl_cns_names <- list(c("vs", "cyl", "carb"), paste0("IVs_", 1:3))
+
+cdl_cns_test <- matrix(c(vs_cns_c, cyl_cns_c, carb_cns_c), nrow = 3, ncol = 3, 
+                   byrow = TRUE, dimnames = cdl_cns_names)
+
+test_obj4 <- domin(mpg ~ vs + cyl + carb, lm, 
+                   list(\(x) list(AIC = extractAIC(x)[[2]]), "AIC"), 
+                   data=mtcars, reverse = TRUE, consmodel = "1") 
+
+test_that("Test Use of Constant Model with Conditional Dominance", {
+  expect_equal(test_obj4$Conditional_Dominance, cdl_cns_test
+  )}
+)
+
+test_that("Test Constant Model Fitstat Value", {
+  expect_equal(test_obj4$Fit_Statistic_Constant_Model, 
+               extractAIC(lm(mpg ~ 1, data = mtcars))[[2]]
+  )}
+)
+
+cyl_vs_cpt <- switch(sum(c(cyl_cns_mgn[[3]] > vs_cns_mgn[[3]]), 
+                         c(cyl_cns_mgn[[1]] > vs_cns_mgn[[1]]))+1, TRUE, NA, FALSE) 
+
+carb_vs_cpt <- switch(sum(c(carb_cns_mgn[[3]] > vs_cns_mgn[[2]]), 
+                          c(carb_cns_mgn[[1]] > vs_cns_mgn[[1]]))+1, TRUE, NA, FALSE)
+
+carb_cyl_cpt <- switch(sum(c(carb_cns_mgn[[2]] > cyl_cns_mgn[[2]]),
+                           c(carb_cns_mgn[[1]] > cyl_cns_mgn[[1]]))+1, TRUE, NA, FALSE)
+
+cpt_rev_test <- matrix(c(NA, cyl_vs_cpt, carb_vs_cpt, 
+                     !cyl_vs_cpt, NA, carb_cyl_cpt, 
+                     !carb_vs_cpt, !carb_cyl_cpt, NA),
+                   nrow = 3, ncol = 3)
+
+dimnames(cpt_rev_test) <- list(
+  paste0("Dmnates_", c("vs", "cyl", "carb")),
+  paste0("Dmnated_", c("vs", "cyl", "carb"))
+)
+
+test_that("Test Reversed Complete Dominance Designation", {
+  expect_equal(test_obj4$Complete_Dominance, cpt_rev_test
+  )})
+
+gen_rev_test <- rank(rowMeans(cdl_cns_test))
+
+names(gen_rev_test) <- cdl_cns_names[[1]]
+
+test_that("Test Reversed General Dominance Ranking", {
+  expect_equal(test_obj4$Ranks, gen_rev_test
+  )}
+)
